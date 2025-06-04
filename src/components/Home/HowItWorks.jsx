e57630ae391db5fd51404194af72de117c6bb673
Muskan-771 <muskanvij771@gmail.com>
2025-06-04 23:24:04 +0530

    adding frontend on git

---
+++ src/components/Home/HowItWorks.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>FROM SEARCH TO SUCCESS !</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Your Account – Start Your Career Journey</p>
+              <p>
+               Sign up in seconds to access a smart, user-friendly platform where job seekers and employers can connect instantly. Your career or hiring success begins here.
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find Jobs / Post Openings – All in One Place</p>
+              <p>
+                    Whether you're seeking your dream role or the perfect hire, Career Connect brings both to one powerful dashboard. Post jobs or explore openings with ease.
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply for Jobs / Recruit Top Talent – Faster, Smarter</p>
+              <p>
+                 Apply to jobs in a click or review the best-fit candidates quickly. Our streamlined system saves time and boosts results for both seekers and recruiters.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
---
+++ src/components/Home/PopularCompanies.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { FaMicrosoft, FaApple } from "react-icons/fa";
+import { SiTesla } from "react-icons/si";
+import { FaGoogle } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { FaAmazon } from "react-icons/fa";
+
+const PopularCompanies = () => {
+  const companies = [
+    {
+      id: 1,
+      title: "Microsoft",
+      // location: "Millennium City Centre, Gurugram",
+      // openPositions: 10,
+      icon: <FaMicrosoft />,
+       link:"https://www.microsoft.com/en-in/about/contact-us",
+    },
+    {
+      id: 2,
+      title: "Google",
+      // location: "Millennium City Centre, Gurugram",
+      // openPositions: 5,
+      icon: <FaGoogle  />,
+      link:"https://www.google.com/about/careers/applications/jobs/results?location=India",
+    },
+    {
+      id: 3,
+      title: "Apple",
+      // location: "Millennium City Centre, Gurugram",
+      // openPositions: 20,
+      icon: <FaAmazon />,
+      link:"https://www.amazon.jobs/content/en/teams/international-stores/india",
+    },
+  ];
+  return (
+    <div className="companies">
+      <div className="container">
+        <h3>TOP COMPANIES</h3>
+        <div className="banner">
+          {companies.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="content">
+                  <div className="icon">{element.icon}</div>
+                  <div className="text">
+                    <h5>{element.title}</h5>
+                    {/* <p>{element.location}</p> */}
+                   
+                  </div>
+                </div>
+                {/* <button>Open Positions {element.openPositions}</button> */}
+                <div className="learn-link-div"> <a
+                  href={element.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="learn-link"
+                 
+                >
+              Dive In
+              {/* Visit */}
+                </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularCompanies;
